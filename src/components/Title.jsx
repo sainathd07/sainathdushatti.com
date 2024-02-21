1e165ed3c13646a884dab6fd77234d3a2c4ddaa7
sainathd07 <saisainath777@gmail.com>
2024-02-21 00:41:02 -0500

    Added Menu Icon from Font Awesome for smaller screens and made it hidden on larger screens

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { FaBars } from "react-icons/fa";
 
 const Title = () => {
   return (
-    <div className='font-body font-bold md:text-[40px] text-[25px]'>Sainath Dushatti</div>
+    <div className=' mt-[30px] flex flex-row justify-between items-center'>
+    <div className='font-body font-bold md:text-[40px] text-[25px]'>Sainath Dushatti</div> {/* My Name */}
+    <FaBars className='text-[25px] md:text-[40px] lg:invisible' /> {/* Hamburger Menu */}
+    </div>
   )
 }
 
