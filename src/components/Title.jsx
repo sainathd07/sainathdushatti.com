90c835acc0328eea96e3280d1975631d7c1a439d
sainathd07 <saisainath777@gmail.com>
2024-02-21 00:50:35 -0500

    changed margin top to margin top and bottom

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import { FaBars } from "react-icons/fa";
 
 const Title = () => {
   return (
-    <div className=' mt-[30px] flex flex-row justify-between items-center'>
+    <div className=' my-[30px] flex flex-row justify-between items-center'>
     <div className='font-body font-bold md:text-[40px] text-[25px]'>Sainath Dushatti</div> {/* My Name */}
     <FaBars className='text-[25px] md:text-[40px] lg:invisible' /> {/* Hamburger Menu */}
     </div>
