2082dcb54d96010f7664b866d78c5cc47d2524df
sainathd07 <saisainath777@gmail.com>
2024-02-21 17:51:36 -0500

    created pages and also implemented the code to make them work when clicked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import Title from './components/Title'
 import Navbar from './components/Navbar'
+import Blog from './pages/Blog'
+import Resume from './pages/Resume'
+import Projects from './pages/Projects'
+import Books from './pages/Books'
+import Papershelf from './pages/Papershelf'
+import Home from './pages/Home'
 
-const App = () => {
+function App() {
+let component
+switch (window.location.pathname) {
+  case "/home":
+    component = <Home />
+    break
+  case "/resume":
+    component = <Resume />
+    break
+  case "/blog":
+    component = <Blog />
+    break
+  case "/projects":
+    component = <Projects />
+    break
+  case "/books":
+    component = <Books />
+    break
+  case "/papershelf":
+    component = <Papershelf />
+    break
+}
+
   return (
     <div className='lg:max-w-[750px] lg:m-auto mx-2'>
     <Title />
     <Navbar />
+    {component}
     </div>
     
   )
---
+++ src/pages/Papershelf.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Papershelf = () => {
+  return (
+    <div>Papershelf</div>
+  )
+}
+
+export default Papershelf
