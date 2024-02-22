436004b01963f503f669ed7134bd4874e216e5dd
sainathd07 <saisainath777@gmail.com>
2024-02-22 17:18:21 -0500

    added home content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='font-body text-[#4A4A4A] text-[22px]'>
+    <p className='font-bold'>Greetings! I am Sainath. I find a problem and build a thing that solves the problem. </p>
+
+    <p>
+    <span>Currently studying computer programming at Youngstown State University, with an expected graduation in January 2025.</span><br />
+    
+    <span>I have previously worked with Persistent Systems helping them manage their codebases.</span><br />
+    
+    <span>I am a passionate web developer and actively building websites. I am fervently diving into the realm of System Design and Cloud Computing with a specific focus on Amazon Web Services (AWS). Currently preparing for certifications that showcase my expertise in cloud technologies.</span><br />
+    
+    <span>You can find me on LinkedIn, Twitter, Instagram, and GitHub, or reach me via email.</span> </p></div>
   )
 }
 
